0efd1c1c4635ed2e266c6809a7dfda85bf3e518c
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-12 14:18:12 +0700

    Simple test
    contain and title

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer')
-const expect = require('chai')
+const expect = require('chai').expect
 
 describe('My first puppeteer test', () => {
   let brower
@@ -24,5 +24,25 @@ describe('My first puppeteer test', () => {
   it('My first test step', async () => {
     await page.goto('https://dev.to/')
     await page.waitForSelector('#nav-search')
+
+    const url = await page.url()
+    const title = await page.title()
+
+    expect(url).to.contain('dev')
+    expect(title).to.contain("Community")
+
+  })
+
+  it('browser reload', async () => {
+    await page.reload()
+    await page.waitForSelector('#page-content')
+
+    const url = await page.url()
+    const title = await page.title()
+
+    expect(url).to.contain('dev')
+    expect(title).to.contain("Community")
+
+
   })
 })
